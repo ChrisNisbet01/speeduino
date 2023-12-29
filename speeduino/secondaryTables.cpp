7b0d64857fd7878a057a818279f9d035f7341366
Chris Nisbet <nischris@gmail.com>
2023-12-30 07:24:39 +1300

    Reduce code duplication in calculateSecondaryFuel()

--- speeduino/secondaryTables.cpp
+++ speeduino/secondaryTables.cpp
@@ -6,72 +6,78 @@
 void calculateSecondaryFuel(void)
 {
   //If the secondary fuel table is in use, also get the VE value from there
-  BIT_CLEAR(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE); //Clear the bit indicating that the 2nd fuel table is in use.
-  if(configPage10.fuel2Mode > 0)
+  BIT_CLEAR(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE);
+  bool fuel2_is_active = false;
+  bool use_base_ve2 = false;
+
+  if(configPage10.fuel2Mode == FUEL2_MODE_MULTIPLY)
   {
-    if(configPage10.fuel2Mode == FUEL2_MODE_MULTIPLY)
+    fuel2_is_active = true;
+    currentStatus.VE2 = getVE2();
+    //Fuel 2 table is treated as a % value. Table 1 and 2 are multiplied together and divided by 100
+    uint16_t combinedVE = ((uint16_t)currentStatus.VE1 * (uint16_t)currentStatus.VE2) / 100;
+    if(combinedVE <= 255) { currentStatus.VE = combinedVE; }
+    else { currentStatus.VE = 255; }
+  }
+  else if(configPage10.fuel2Mode == FUEL2_MODE_ADD)
+  {
+    fuel2_is_active = true;
+    currentStatus.VE2 = getVE2();
+    //Fuel tables are added together, but a check is made to make sure this won't overflow the 8-bit VE value
+    uint16_t combinedVE = (uint16_t)currentStatus.VE1 + (uint16_t)currentStatus.VE2;
+    if(combinedVE <= 255) { currentStatus.VE = combinedVE; }
+    else { currentStatus.VE = 255; }
+  }
+  else if(configPage10.fuel2Mode == FUEL2_MODE_CONDITIONAL_SWITCH )
+  {
+    if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_RPM)
+    {
+      if(currentStatus.RPM > configPage10.fuel2SwitchValue)
+      {
+        use_base_ve2 = true;
+      }
+    }
+    else if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_MAP)
+    {
+      if(currentStatus.MAP > configPage10.fuel2SwitchValue)
+      {
+        use_base_ve2 = true;
+      }
+    }
+    else if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_TPS)
+    {
+      if(currentStatus.TPS > configPage10.fuel2SwitchValue)
+      {
+        use_base_ve2 = true;
+      }
+    }
+    else if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_ETH)
+    {
+      if(currentStatus.ethanolPct > configPage10.fuel2SwitchValue)
+      {
+        use_base_ve2 = true;
+      }
+    }
+  }
+  else if(configPage10.fuel2Mode == FUEL2_MODE_INPUT_SWITCH)
+  {
+    if(digitalRead(pinFuel2Input) == configPage10.fuel2InputPolarity)
+    {
+      use_base_ve2 = true;
+    }
+  }
+  else
+  {
+    /* Do nothing. */
+  }
+
+  if (fuel2_is_active || use_base_ve2)
+  {
+    BIT_SET(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE);
+    if (use_base_ve2)
     {
       currentStatus.VE2 = getVE2();
-      //Fuel 2 table is treated as a % value. Table 1 and 2 are multiplied together and divided by 100
-      uint16_t combinedVE = ((uint16_t)currentStatus.VE1 * (uint16_t)currentStatus.VE2) / 100;
-      if(combinedVE <= 255) { currentStatus.VE = combinedVE; }
-      else { currentStatus.VE = 255; }
-    }
-    else if(configPage10.fuel2Mode == FUEL2_MODE_ADD)
-    {
-      currentStatus.VE2 = getVE2();
-      //Fuel tables are added together, but a check is made to make sure this won't overflow the 8-bit VE value
-      uint16_t combinedVE = (uint16_t)currentStatus.VE1 + (uint16_t)currentStatus.VE2;
-      if(combinedVE <= 255) { currentStatus.VE = combinedVE; }
-      else { currentStatus.VE = 255; }
-    }
-    else if(configPage10.fuel2Mode == FUEL2_MODE_CONDITIONAL_SWITCH )
-    {
-      if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_RPM)
-      {
-        if(currentStatus.RPM > configPage10.fuel2SwitchValue)
-        {
-          BIT_SET(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE); //Set the bit indicating that the 2nd fuel table is in use.
-          currentStatus.VE2 = getVE2();
-          currentStatus.VE = currentStatus.VE2;
-        }
-      }
-      else if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_MAP)
-      {
-        if(currentStatus.MAP > configPage10.fuel2SwitchValue)
-        {
-          BIT_SET(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE); //Set the bit indicating that the 2nd fuel table is in use.
-          currentStatus.VE2 = getVE2();
-          currentStatus.VE = currentStatus.VE2;
-        }
-      }
-      else if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_TPS)
-      {
-        if(currentStatus.TPS > configPage10.fuel2SwitchValue)
-        {
-          BIT_SET(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE); //Set the bit indicating that the 2nd fuel table is in use.
-          currentStatus.VE2 = getVE2();
-          currentStatus.VE = currentStatus.VE2;
-        }
-      }
-      else if(configPage10.fuel2SwitchVariable == FUEL2_CONDITION_ETH)
-      {
-        if(currentStatus.ethanolPct > configPage10.fuel2SwitchValue)
-        {
-          BIT_SET(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE); //Set the bit indicating that the 2nd fuel table is in use.
-          currentStatus.VE2 = getVE2();
-          currentStatus.VE = currentStatus.VE2;
-        }
-      }
-    }
-    else if(configPage10.fuel2Mode == FUEL2_MODE_INPUT_SWITCH)
-    {
-      if(digitalRead(pinFuel2Input) == configPage10.fuel2InputPolarity)
-      {
-        BIT_SET(currentStatus.status3, BIT_STATUS3_FUEL2_ACTIVE); //Set the bit indicating that the 2nd fuel table is in use.
-        currentStatus.VE2 = getVE2();
-        currentStatus.VE = currentStatus.VE2;
-      }
+      currentStatus.VE = currentStatus.VE2;
     }
   }
 }
