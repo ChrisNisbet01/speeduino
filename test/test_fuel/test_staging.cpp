d280567098ab28a93bd2d25d9bbc24340d6269f4
Chris Nisbet <nischris@gmail.com>
2024-02-25 09:16:30 +1300

    fixed comment relating to fuel staging test in auto mode.

--- test/test_fuel/test_staging.cpp
+++ test/test_fuel/test_staging.cpp
@@ -68,7 +68,7 @@ void test_Staging_4cyl_Auto_Inactive(void)
   uint32_t pwLimit = 9000; //90% duty cycle at 6000rpm
   calculateInjectorStaging(testPW, pwLimit);
   // PW 1 and 2 should be normal, 3 and 4 should be 0 as that testPW is below the pwLimit
-  // PW1/2 should be (PW - openTime) * staged_req_fuel_mult_pri = openTime + (3000 - 1000) * 3.0 = 7000
+  // PW1/2 should be ((PW - openTime) * staged_req_fuel_mult_pri) + openTime = ((3000 - 1000) * 3.0) + 1000 = 7000
   TEST_ASSERT_FALSE(BIT_CHECK(currentStatus.status4, BIT_STATUS4_STAGING_ACTIVE));
   TEST_ASSERT_EQUAL(7000, injectors.injector(injChannel1).PW);
   TEST_ASSERT_EQUAL(7000, injectors.injector(injChannel2).PW);
